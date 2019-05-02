ddc318edcea93d99d7bbbb883307e5b537fe5b66
Patrick <hollorpj@gmail.com>
2019-05-02 00:37:56 -0400

    Preliminary skin. File uploader. Displaying uploaded image

--- src/app/components/selector-pane/selector-pane.component.ts
+++ src/app/components/selector-pane/selector-pane.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'selector-pane',
   templateUrl: './selector-pane.component.html',
   styleUrls: ['./selector-pane.component.css']
 })
-export class SelectorPaneComponent implements OnInit {
+export class SelectorPaneComponent  {
+
+  @ViewChild('spriteUploader')
+  private spriteUploader;
+
+  private temp;
 
   constructor() { }
 
-  ngOnInit() {
+  private openSpriteUploader() {
+    let event = new MouseEvent('click', {bubbles: false});
+    this.spriteUploader.nativeElement.dispatchEvent(event);
   }
 
+  public importSpritesheet(spritesheetFile) {
+    let file = spritesheetFile.target.files[0];
+
+    var fileReader = new FileReader();
+    var that = this;
+    fileReader.onload = function(e) {
+       that.temp = fileReader.result;
+    }
+    fileReader.readAsDataURL(file);
+  }
 }
